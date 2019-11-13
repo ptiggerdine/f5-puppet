d2ea1bdaa9aff99c029f04f278bf251ec92fd544
Christian Hoefig <christian.hoefig@ext.parshipgroup.com>
2019-11-13 14:39:55 +0100

    no verbose flag

---
+++ lib/puppet/provider/f5_command_verbose/rest.rb
@@ -0,0 +1,39 @@
+require File.join(File.dirname(__FILE__), '../f5')
+require 'json'
+
+Puppet::Type.type(:f5_command_verbose).provide(:rest, parent: Puppet::Provider::F5) do
+
+  def message(object)
+    # Allows us to pass in resources and get all the attributes out
+    # in the form of a hash.
+    message = object.to_hash
+
+    # Map for conversion in the message.
+    map = {
+    }
+
+   command = message[:tmsh]
+   command ="-c " + "\"" + command +"\""
+   message = {"command"=> "run", "utilCmdArgs"=> command }
+
+  message.to_json
+  end
+
+  def exists?
+    @property_hash[:ensure] == :present
+  end
+
+  def create
+    result = Puppet::Provider::F5.post("/mgmt/tm/util/bash", message(resource))
+    # We clear the hash here to stop flush from triggering.
+    @property_hash.clear
+    m = resource.to_hash
+    Puppet.notice "TMSH: description " + m[:description] if m[:description]
+    rb = JSON.parse(result.body)
+    Puppet.warn "TMSH commandResult " + rb['commandResult'].chomp if rb['commandResult']
+    return result
+  end
+
+  mk_resource_methods
+
+end
---
+++ lib/puppet/type/f5_command_verbose.rb
@@ -0,0 +1,29 @@
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/parameter/f5_name.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_address.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_availability_requirement.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_connection_limit.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_connection_rate_limit.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_description.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_health_monitors.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_ratio.rb'))
+require File.expand_path(File.join(File.dirname(__FILE__),'..','..','puppet/property/f5_state.rb'))
+
+Puppet::Type.newtype(:f5_command) do
+  @doc = 'Run tmsh command'
+
+  apply_to_device
+
+  ensurable do
+    defaultvalues
+    defaultto :present
+  end
+
+  newparam(:name, :parent => Puppet::Parameter::F5Name, :namevar => true)
+
+  newproperty(:description, :parent => Puppet::Property::F5Description)
+
+  newproperty(:tmsh) do
+    desc "tmsh command line"
+  end
+
+end
