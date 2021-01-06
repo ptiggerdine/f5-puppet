b46f90af1d372793e76e041ae34ab9a9230cf706
ptiggerdine <ptiggerdine [at] gmail.com>
2021-01-06 21:09:17 +1000

    CCD-1804 added additional tests for new fields.

--- spec/acceptance/f5_profileclientssl/rest_spec.rb
+++ spec/acceptance/f5_profileclientssl/rest_spec.rb
@@ -4,11 +4,15 @@ describe 'f5_profileclientssl' do
   it 'creates and updates serverssl-profile1' do
     pp=<<-EOS
     f5_profileclientssl {'/Common/clientssl-profile1':
-       ensure                          => 'present',
-       cert                            =>"/Common/default.crt",
-       key                             =>"/Common/default.key",
-       proxy_ssl                        => 'enabled',
-       proxy_ssl_passthrough           => 'enabled',
+       ensure                => 'present',
+       defaults_from         => '/Common/clientssl',
+       cert                  => '/Common/default.crt',
+       key                   => '/Common/default.key',
+       proxy_ssl             => 'enabled',
+       proxy_ssl_passthrough => 'enabled',
+       cipher_group:         => '/Common/f5-default',
+       ciphers:              => 'DEFAULT',
+       ssl_options:          => '{ dont-insert-empty-fragments no-sslv3 no-tlsv1 no-tlsv1.1 }'
     }
     EOS
     make_site_pp(pp)
@@ -29,14 +33,14 @@ describe 'f5_profileclientssl' do
     # EOS
     # make_site_pp(pp2)
     # run_device(:allow_changes => true)
-    # run_device(:allow_changes => false)    
+    # run_device(:allow_changes => false)
   end
 
   it 'deletes f5_profileclientssl' do
     pp=<<-EOS
     f5_profileclientssl {'/Common/clientssl-profile1':
       ensure => 'absent',
-    }    
+    }
     EOS
     make_site_pp(pp)
     run_device(:allow_changes => true)
@@ -44,4 +48,3 @@ describe 'f5_profileclientssl' do
 
   end
 end
-
